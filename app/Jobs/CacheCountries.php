ba6cdb42755e1dacf3a648c30742713ac652f896
i.pismak <i.pismak@corp.mail.ru>
2019-07-23 16:03:35 +0300

    commit

---
+++ app/Jobs/CacheCountries.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
+
+class CacheCountries implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    const STATS_PRIMARY_KEY   = 'stats.countries.primary';
+    const STATS_SECONDARY_KEY = 'stats.countries.secondary';
+    const PARENT_KEY          = 'countries:';
+    const KEY_MASK            = self::PARENT_KEY . '%s';
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Собираем по ключам из хранилища
+        $keys      = Redis::keys(self::PARENT_KEY . '*');
+        $countries = [];
+        foreach ($keys as $key) {
+            $value                 = Redis::get($key);
+            $outputKey             = str_replace(self::PARENT_KEY, '', $key);
+            $countries[$outputKey] = $value;
+        }
+        // Кладем в кэш, время жизни выставляется в общем конфиге (тип хранилища тоже можно выбрать в конфиге (Memcached, Redis, File))
+        Cache::put(self::STATS_PRIMARY_KEY, $countries, getenv('CACHE_LIFE_TIME_PRIMARY'));
+        // Дублируем данные в более долгоживущий кэш (можно бессмертный)
+        Cache::put(self::STATS_SECONDARY_KEY, $countries, getenv('CACHE_LIFE_TIME_SECONDARY'));
+    }
+}
--- app/Services/CountryService.php
+++ app/Services/CountryService.php
@@ -4,34 +4,40 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Queue;
+
+use App\Jobs\CacheCountries;
+
 
 class CountryService
 {
-    const STATS_KEY  = 'stats.countries';
-    const PARENT_KEY = 'countries:';
-    const KEY_MASK   = self::PARENT_KEY . '%s';
+    const STATS_PRIMARY_KEY   = 'stats.countries.primary';
+    const STATS_SECONDARY_KEY = 'stats.countries.secondary';
+    const PARENT_KEY          = 'countries:';
+    const KEY_MASK            = self::PARENT_KEY . '%s';
 
     /**
      * @return array
      */
     public function list()
     {
-        // Если есть в кэше - забираем стату оттуда, региулируем время жизни с помощью TTL в конфиге (как бы слейв)
-        if ($stats = Cache::get(self::STATS_KEY)) {
+        // Если есть в первичном кэше - забираем стату оттуда, регулируем время жизни с помощью TTL в конфиге (как бы слейв)
+        if ($stats = Cache::get(self::STATS_PRIMARY_KEY)) {
             return $stats;
         }
-        // Если в кэше нет - собираем по ключам из хранилища
-        $keys      = Redis::keys(self::PARENT_KEY . '*');
-        $countries = [];
-        foreach ($keys as $key) {
-            $value                 = Redis::get($key);
-            $outputKey             = str_replace(self::PARENT_KEY, '', $key);
-            $countries[$outputKey] = $value;
-        }
-        // Кладем в кэш, время жизни выставляется в общем конфиге (тип хранилища тоже можно выбрать в конфиге (Memcached, Redis, File))
-        Cache::put(self::STATS_KEY, $countries, getenv('CACHE_LIFE_TIME'));
 
-        return $countries;
+        // Подключаем сервис очередей, для фоновой обработки
+        $cacheService = new CacheCountries();
+
+        // Если есть в запасном кэше - забираем стату из него => отвечаем асинхронно => пишем в оба кэша фоном с помощью очередей,
+        if ($stats = Cache::get(self::STATS_SECONDARY_KEY)) {
+            Queue::push($cacheService);
+        // Иначе делаем запись в кэши в риалтайме (истекло время ожидания запасного кэша, чего быть не должно при правильной настройке)
+        } else {
+            $cacheService->handle();
+        }
+
+        return $stats;
     }
 
     /**
